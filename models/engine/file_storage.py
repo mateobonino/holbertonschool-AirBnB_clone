3c6225d3cc326dc85e52a8482fb22d267a37de9f
mateo <mateo@localhost.localdomain>
2022-10-11 13:56:49 -0300

    Task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,9 +29,7 @@ class FileStorage():
                 with open(FileStorage.__file_path, 'r') as f:
                     json_dict = json.loads(f)
                     for i, j in json_dict.items():
-                        
-                    
-                        
+                        items = eval(j['__class_'])(**j)
+                        FileStorage.__objects[i] = items
         except FileNotFoundError:
             pass
-

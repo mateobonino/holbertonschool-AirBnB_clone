53d608d3f9614c07426a24b277d047a8bcdad450
root <root@H45.hbtn.io>
2022-10-11 13:26:24 -0300

    Add function in console

--- console.py
+++ console.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3
 import cmd
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
-    def EOF():
-        quit()
+    prompt = '(hbnb)'
+
+    def EOF(self):
+        exit()
+
+    def do_quit(self):
+        exit()
+
+    def help_EOF(self):
+        print("Quit command to exit the program\n")
+
+    def help_quit(self):
+        print("Quit commando to exit the program\n")
+
+    def emptyline():
+        pass
 
         if  __name__ == '__main__':
             HBNBCommand().cmdloop()

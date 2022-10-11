24bdec33e9e482c37652b13a950fc6ba01ff8b87
Lautaro1387 <lautaro12387@gmail.com>
2022-10-11 06:11:03 -0700

    Task 5 (not functional)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 
+
 class FileStorage():
         __file_path = 'file.json'
         __objects = {}
+
+    def all(self):
+        dics = {}
+        return self.dics
+
+    def new(self, obj):
+        self.obj
+
+    def save(self):
+        with open(__file_path, 'w') as f:
+            f = json.dump(__file_path, f)
+
+    def reload(self):
+        __objects

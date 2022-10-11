9d17a9d893d6e94d4fc19a0f8d1b6ab205a79d4d
root <root@H45.hbtn.io>
2022-10-11 14:24:14 -0300

    Fix

--- console.py
+++ console.py
@@ -9,25 +9,23 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-
 class HBNBCommand(cmd.Cmd):
     """Command interpreter"""
     prompt = '(hbnb)'
 
-    def EOF(self):
+    def EOF(self, args):
+        print("Quit command to exit the program\n")
         exit()
 
-    def do_quit(self):
+    def do_quit(self, args):
+        print("Quit command to exit the program\n")
         exit()
 
     def help_EOF(self):
         print("Quit command to exit the program\n")
 
-    def help_quit(self):
-        print("Quit commando to exit the program\n")
-
     def emptyline():
         pass
 
-        if  __name__ == '__main__':
-            HBNBCommand().cmdloop()
+if  __name__ == '__main__':
+    HBNBCommand().cmdloop()

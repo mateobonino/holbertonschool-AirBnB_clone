953ba2abe1081020a67e73fafed1193cf3a646cf
mateo <mateo@localhost.localdomain>
2022-10-11 16:37:04 -0300

    Task 5

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)

952533d393d00749a6bfed04e1d10dfbbe7bef54
mateo <mateo@localhost.localdomain>
2022-10-11 19:24:38 -0300

    Task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,12 +17,12 @@ class FileStorage():
         FileStorage.__objects[key] = obj.to_dict()
 
     def save(self):
-        new_dict = {}
+        """ new_dict = {}
 
         for i in self.__objects:
-            new_dict[i] = self.__objects[i].to_dict()
+            new_dict[i] = self.__objects[i].to_dict() """
         with open(FileStorage.__file_path, 'w') as f:
-            f.write(json.dumps(new_dict, default=str))
+            f.write(json.dumps(self.__objects, default=str))
 
     def reload(self):
         try:

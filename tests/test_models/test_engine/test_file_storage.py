fe67fa71a0e4a1ec93e3e9bf406d1a8ce652b866
mateo <mateo@localhost.localdomain>
2022-10-13 15:53:21 -0300

    Task 5 Tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -11,8 +11,8 @@ class TestFileStorage(unittest.TestCase):
         obj = FileStorage()
         self.assertEqual(obj._FileStorage__file_path, "file.json")
 
-    def test_all(self):
-        self.assertEqual(dict, type(FileStorage.all(self)))
-
     def test_objects(self):
         self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_reload(self):
+        self.assertTrue(hasattr(FileStorage, 'reload'), True)

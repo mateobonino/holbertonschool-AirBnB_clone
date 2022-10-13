b419bd977026d83a2982299c5a143f0e796650f5
mateo <mateo@localhost.localdomain>
2022-10-13 13:34:24 -0300

    Task 5 Tests

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from models.engine.file_storage import FileStorage
+import unittest
+
+class TestFileStorage(unittest.TestCase):
+    def test_all(self):
+        obj = FileStorage()
+        self.assertTrue(hasattr(obj, 'all'), True)
+
+    def test_save(self):
+        obj = FileStorage()
+        self.assertTrue(hasattr(obj, 'save'), True)
+
+    def test_new(self):
+        obj = FileStorage()
+        self.assertTrue(hasattr(obj, 'new'), True)
+
+    def test_reload(self):
+        obj = FileStorage()
+        self.assertTrue(hasattr(obj, 'reload'), True)
+
+    def test__file_path(self):
+        obj = FileStorage()
+        self.assertEqual(obj._FileStorage__file_path, "file.json")

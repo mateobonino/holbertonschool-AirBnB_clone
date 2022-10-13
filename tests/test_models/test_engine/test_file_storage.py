a3f98784e7bc9e4b8d98dc8e59fdddf0ddcc1dd4
mateo <mateo@localhost.localdomain>
2022-10-13 14:29:42 -0300

    Task 5 Tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,11 +4,6 @@ from models.engine.file_storage import FileStorage
 import unittest
 
 class TestFileStorage(unittest.TestCase):
-    def test_all(self):
-        obj = FileStorage()
-        obj1 = FileStorage()
-        self.assertEqual(obj._FileStorage__objects, obj1._FileStorage__objects)
-
     def test_save(self):
         obj = FileStorage()
         self.assertTrue(hasattr(obj, 'save'), True)
@@ -20,3 +15,6 @@ class TestFileStorage(unittest.TestCase):
     def test__file_path(self):
         obj = FileStorage()
         self.assertEqual(obj._FileStorage__file_path, "file.json")
+
+    def test_all(self):
+        self.assertEqual(dict, type(FileStorage.all(self)))

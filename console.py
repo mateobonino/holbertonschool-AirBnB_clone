05d646c0cfded30e5ccbc0d7962f3600429b7914
root <root@H45.hbtn.io>
2022-10-13 10:27:42 -0300

    Fix class destroy and show

--- console.py
+++ console.py
@@ -26,13 +26,12 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
-    def help(self):
-        print("Quit command to exit the program\n")
-    
     def emptyline(self):
+        """empty line"""
         pass
 
     def do_create(self, arg):
+        """Create new instance of BaseModel"""
         if not arg:
             print("** class name missing **")
         else:
@@ -44,6 +43,9 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
 
     def do_show(self, arg):
+        """
+        The string representation of an instance based on the class name
+        """
         var = arg.split(" ")
         if len(var) == 1 and var[0] == "":
             print("** class name missing **")
@@ -55,12 +57,13 @@ class HBNBCommand(cmd.Cmd):
             else:
                 my_dict = storage.all()
                 for obj in my_dict:
-                    var2 = arg.split(".")
-                    if var2[0] == var[0]:
+                    var2 = obj.split(".")
+                    if var[1] == var2[1]:
                         print("{}".format(my_dict[obj]))
                 print("** no instance found **")
 
     def do_destroy(self, arg):
+        """Deletes an instance based on the class name"""
         var = arg.split(" ")
         if len(var) == 1 and var[0] == " ":
             print("** class name missing **")
@@ -72,10 +75,13 @@ class HBNBCommand(cmd.Cmd):
             else:
                 my_dict = storage.all()
                 for obj in my_dict:
-                    my_dict.pop(obj)
+                    var2 = obj.split(" ")
+                    if var2[1] == var[1]:
+                        my_dict.pop(obj)
             print("** no instance found **")
 
     def do_all(self, arg):
+        """Prints all string representation based or not on the class name"""
         if arg:
             if arg not in self.ClassList:
                 print("** class doesn't exist **")
@@ -85,6 +91,7 @@ class HBNBCommand(cmd.Cmd):
                     print("{}".format(my_dict[obj]))
 
     def do_update(self,arg):
+        """Updates an instance based on the class name"""
         var = arg.split(" ")
         if len(var) == 1:
             print("** class name missing **")

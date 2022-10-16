401734d9b343cb9837e1315d83cbd193b68164e1
Mateo Bonino <mateo@localhost.localdomain>
2022-10-15 23:59:13 -0300

    Tests Documentation

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Test cases for BaseModel class"""
 
 import unittest
 from models.base_model import BaseModel
@@ -9,6 +9,7 @@ from datetime import datetime
 class TestBaseModel(unittest.TestCase):
 
     def test_save(self):
+        """Tests the save method"""
         obj = BaseModel()
         test1 = obj.created_at
         test2 = obj.updated_at
@@ -17,9 +18,11 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(test2, obj.updated_at)
 
     def test_str(self):
+        """Tests the __str__ method"""
         self.assertEqual(str, type(str(BaseModel)))
 
     def test_to_dict(self):
+        """Tests the to_dict method"""
         obj = BaseModel()
         obj_dict = {'id': obj.id, 'created_at': obj.created_at, 'updated_at': obj.updated_at, '__class__': obj.__class__.__name__}
         # self.assertEqual(obj.to_dict(), '')
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
+"""Test cases for User Class"""
+
 
 from models.user import User
 import unittest
 
 
 class TestUser(unittest.TestCase):
+    """Test cases for User Class"""
     def test_user_email(self):
+        """Checks if the email attribute is a string"""
         self.assertEqual(str, type(User.email))
 
     def test_user_password(self):
+        """Checks if the password attribute is a string"""
         self.assertEqual(str, type(User.password))
 
     def test_user_first_name(self):
+        """Checks if the user first name attribute is a string"""
         self.assertEqual(str, type(User.first_name))
 
     def test_user_last_name(self):
+        """Checks if the user last name attribute is a string"""
         self.assertEqual(str, type(User.last_name))

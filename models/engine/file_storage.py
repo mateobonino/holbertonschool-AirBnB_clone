65b661c2a22129e77e4c8b0381f38453467e88d7
Mateo Bonino <mateo@localhost.localdomain>
2022-10-15 23:48:43 -0300

    File storage documentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""File Storage Class"""
 
 import json
 from models.base_model import BaseModel
@@ -7,17 +7,21 @@ import os.path
 
 
 class FileStorage():
+    """Manages the object serialization and deserialization to JSON"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """Returns all the objects stored"""
         return FileStorage.__objects
 
     def new(self, obj):
+        """Creates a new objects"""
         key = obj.__class__.__name__ + '.' + obj.id
         FileStorage.__objects[key] = obj
 
     def save(self):
+        """Saves the objects to JSON"""
         new_dict = {}
         for i in self.__objects:
             new_dict[i] = self.__objects[i].to_dict()
@@ -25,6 +29,7 @@ class FileStorage():
             f.write(json.dumps(new_dict, default=str))
 
     def reload(self):
+        """Loads a JSON file and recreates the object"""
         from models.base_model import BaseModel
         from models.amenity import Amenity
         from models.city import City

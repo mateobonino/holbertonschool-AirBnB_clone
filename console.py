053568867ea11a62185abf1909d061dc515950bb
Lautaro1387 <lautaro12387@gmail.com>
2022-10-15 18:02:29 -0700

    Fix task 10

--- console.py
+++ console.py
@@ -84,7 +84,7 @@ class HBNBCommand(cmd.Cmd):
                 return
             for obj in my_dict:
                 line2 = obj.split(".")
-                if line2[1] == line[1]:
+                if line2[1] == line[1] and line2[0] == line[0]:
                     print("{}".format(my_dict[obj]))
                     return
             print("** no instance found **")
